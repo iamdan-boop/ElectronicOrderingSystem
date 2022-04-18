2f0dca8ed033c9427a7687558787b17075aab10d
Kenji Velasco <thedeceptivesanta@gmail.com>
2022-04-18 17:08:23 +0800

    initial_commit

---
+++ app/src/main/java/com/sti/canteen_ordering_app/utils/Result.kt
@@ -0,0 +1,7 @@
+package com.sti.canteen_ordering_app.utils
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Loading<T>(isLoading: Boolean = true, )
+    class Success<T>(data: T) : Resource<T>(data = data)
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data = data, message = message)
+}
